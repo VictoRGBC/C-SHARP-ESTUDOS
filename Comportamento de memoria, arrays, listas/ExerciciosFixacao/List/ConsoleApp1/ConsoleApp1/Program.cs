968c4f5119701328a7ac9841525930fd2c889bea
Victor <victorgabtec@gmail.com>
2025-07-17 22:32:52 -0300

    Estudos
    OOP

---
+++ Comportamento de memoria, arrays, listas/ExerciciosFixacao/List/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Ana");
+
+            list.Insert(2, "Marco");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("List count: " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First 'A' position: " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last 'A' position: " + pos2);
+
+
+            Console.WriteLine();
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            foreach (string item in list2)
+            {
+                Console.WriteLine("5-letter name: " + item);
+            }
+
+            list.Remove("Alex");
+            Console.WriteLine();
+            foreach (string item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.RemoveAt(3);
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.RemoveRange(0, 2);
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Comportamento de memoria, arrays, listas/ExerciciosFixacao/Matriz/Ex2/Ex2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split(' ');
+            int M = int.Parse(num[0]);
+            int N = int.Parse(num[1]);
+
+            int[,] mat = new int[M, N];
+
+            for (int  i = 0;  i < M;  i++)
+            {
+                string[] row = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i,j] = int.Parse(row[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < N - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < M - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
