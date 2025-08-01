87b61c3e389d4c465c82d1e4cf3a7c8903fba194
Victor <victorgabtec@gmail.com>
2025-08-01 20:09:10 -0300

    Estudos Recursvidade e Interface Grafica

---
+++ 12 - Recursividade/Fatorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("O fatorial de" + n + " é " + Fatorial(n));
+
+            Console.ReadKey();
+        }
+
+        static int Fatorial(int n)
+        {
+            if ( n <= 0 )
+            {
+                return 1;
+            }
+            return n * Fatorial(n - 1);
+        }
+    }
+}
---
+++ 13 - Aplicacao Windows Forms/FormTeste/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace FormTeste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Executar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Executar
+            // 
+            this.Executar.Location = new System.Drawing.Point(708, 411);
+            this.Executar.Name = "Executar";
+            this.Executar.Size = new System.Drawing.Size(80, 27);
+            this.Executar.TabIndex = 0;
+            this.Executar.Text = "Executar";
+            this.Executar.UseVisualStyleBackColor = true;
+            this.Executar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Executar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minha Aplicação";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Executar;
+    }
+}
+
